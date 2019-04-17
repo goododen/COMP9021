f5db7694249fb49d26b271381da317106da777e5
Odenpage <48947316+goododen@users.noreply.github.com>
2019-04-18 00:11:04 +1000

    Add files via upload

--- quizes/quiz_1.py
+++ quizes/quiz_1.py
@@ -1,5 +1,3 @@
-# Written by *** and Eric Martin for COMP9021
-
 
 '''
 See https://en.wikipedia.org/wiki/Deterministic_finite_automaton
@@ -19,11 +17,10 @@ accepts words with an even number of occurrences of 0's.
 
 '''
 
-
 def describe_automaton(transitions):
     '''
     The output is produced with the print() function.
-    
+
     >>> transitions_1 = {('q0', 0): 'q1', ('q1', 1): 'q0'}
     >>> describe_automaton(transitions_1)
     When in state "q0" and processing "0", automaton's state becomes "q1".
@@ -37,15 +34,15 @@ def describe_automaton(transitions):
     When in state "state_2" and processing "0", automaton's state becomes "state_1".
     When in state "state_2" and processing "1", automaton's state becomes "state_2".
     '''
-    pass
-    # REPLACE pass ABOVE WITH YOUR CODE
+    for k,v in transitions.items():
+        print(f'When in state "{k[0]}" and processing "{k[1]}", automaton\'s state becomes "{v}".')
 
 def transitions_as_dict(transitions_as_list):
     '''
     transitions_as_list is a list of strings of the form 'state_1,symbol:state_2'
     where 'state_1' and 'state_2' are words and 'symbol' is one of the 10 digits.
     We assume that there is at most one 'state_2' for given 'state_1' and 'symbol'.
-    
+
     >>> transitions_as_dict(['q0,0:q1', 'q1,1:q0'])
     {('q0', 0): 'q1', ('q1', 1): 'q0'}
     >>> transitions_as_dict(['state_1,0:state_2', 'state_1,1:state_1',\
@@ -54,7 +51,13 @@ def transitions_as_dict(transitions_as_list):
 ('state_2', 0): 'state_1', ('state_2', 1): 'state_2'}
     '''
     transitions = {}
-    # INSERT YOUR CODE HERE
+    for i in transitions_as_list:
+        i = i.split(':')
+        key = i[0].split(',')
+        key = (key[0], int(key[1]))
+        value = i[1]
+        # print(key,value)
+        transitions[key] = value
     return transitions
 
 def accepts(transitions, word, initial_state, accept_state):
@@ -62,8 +65,8 @@ def accepts(transitions, word, initial_state, accept_state):
     Starting in 'initial_state', if the automaton can process with 'transitions'
     all symbols in 'word' and eventually reach 'accept_state', then the function
     returns True; otherwise it returns False.
-    
-    >>> transitions_1 = {('q0', 0): 'q1', ('q1', 1): 'q0'} 
+
+    >>> transitions_1 = {('q0', 0): 'q1', ('q1', 1): 'q0'}
     >>> accepts(transitions_1, '00', 'q0', 'q1')
     False
     >>> accepts(transitions_1, '2', 'q0', 'q0')
@@ -87,10 +90,26 @@ def accepts(transitions, word, initial_state, accept_state):
     >>> accepts(transitions_2, '10111000101', 'state_1', 'state_1')
     False
     '''
-    pass
-    # REPLACE pass ABOVE WITH YOUR CODE
-
-    
+    current =  initial_state
+    for w in word:
+        try:
+            current = transitions[(current,int(w))]
+        except:
+            return False
+    if current != accept_state:
+        return False
+    return True
+ 
+current_state=initial_state
+for m in word:
+    if (current_state, int(m)) in transistions:
+        current_state=transitions[current_state, int(m)]
+        else:
+            return False
+        if current_state==accept_state:
+            return True
+        else:
+            return False
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
